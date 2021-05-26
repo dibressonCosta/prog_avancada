23f14e5b8aa696caf7c1e5330015ea0628a50e26
dibressonCosta <luizgustavo.costa@hotmail.com>
2021-05-25 23:56:22 -0300

    inicio popup evento

--- agenda/src/components/Agenda.vue
+++ agenda/src/components/Agenda.vue
@@ -67,9 +67,7 @@
               </v-btn>
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
               <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
+              
             </v-toolbar>
             <v-card-text>
               <span v-html="selectedEvent.details"></span>
