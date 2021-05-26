2a014ba25629100fd02b61b1190a0c420802a4e2
Gusta <gusta@gusta.monster>
2021-05-25 23:27:52 -0300

    agenda sem error

--- agenda/src/components/Agenda.vue
+++ agenda/src/components/Agenda.vue
@@ -9,7 +9,7 @@
           <v-btn fab text small color="grey darken-2" @click="prev">
             <v-icon small> mdi-chevron-left </v-icon>
           </v-btn>
-          <v-btn fab text small color="grey darken-2" @click="next">
+          <v-btn fab text small color="grey darken-2" @click="next" class="mr-4">
             <v-icon small> mdi-chevron-right </v-icon>
           </v-btn>
           <v-toolbar-title v-if="$refs.calendar">
@@ -49,7 +49,6 @@
           :events="events"
           :event-color="getEventColor"
           :type="type"
-          :now="today"
           @click:event="showEvent"
           @click:more="viewDay"
           @click:date="viewDay"
@@ -101,17 +100,19 @@ export default {
       day: "Dia",
       "4day": "4 dias",
     },
-    titulo: null,
-    detalhes: null,
-    inicio: null,
-    final: null,
-    cor: "#1976d2",
+    name: null,
+    details: null,
+    start: null,
+    end: null,
+    color: "#1976d2",
     currentlyEditing: null,
     selectedEvent: {},
-    selectedElement: {},
+    selectedElement: null,
     selectedOpen: false,
     events: [],
     dialog: false,
+    names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
   }),
   created: function () {
     this.fetchUsuarioData();
@@ -124,7 +125,68 @@ export default {
           this.events = response.body
         },
       )
-    }
+    },
+    viewDay ({ date }) {
+        this.focus = date
+        this.type = 'day'
+      },
+      getEventColor (event) {
+        return event.color
+      },
+      setToday () {
+        this.focus = ''
+      },
+      prev () {
+        this.$refs.calendar.prev()
+      },
+      next () {
+        this.$refs.calendar.next()
+      },
+      showEvent ({ nativeEvent, event }) {
+        const open = () => {
+          this.selectedEvent = event
+          this.selectedElement = nativeEvent.target
+          requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
+        }
+
+        if (this.selectedOpen) {
+          this.selectedOpen = false
+          requestAnimationFrame(() => requestAnimationFrame(() => open()))
+        } else {
+          open()
+        }
+
+        nativeEvent.stopPropagation()
+      },
+      updateRange ({ start, end }) {
+        const events = []
+
+        const min = new Date(`${start.date}T00:00:00`)
+        const max = new Date(`${end.date}T23:59:59`)
+        const days = (max.getTime() - min.getTime()) / 86400000
+        const eventCount = this.rnd(days, days + 20)
+
+        for (let i = 0; i < eventCount; i++) {
+          const allDay = this.rnd(0, 3) === 0
+          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+          const second = new Date(first.getTime() + secondTimestamp)
+
+          events.push({
+            name: this.names[this.rnd(0, this.names.length - 1)],
+            start: first,
+            end: second,
+            color: this.colors[this.rnd(0, this.colors.length - 1)],
+            timed: !allDay,
+          })
+        }
+
+        this.events = events
+      },
+      rnd (a, b) {
+        return Math.floor((b - a + 1) * Math.random()) + a
+      },
   }
 };
 </script>
