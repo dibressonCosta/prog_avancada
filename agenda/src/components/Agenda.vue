5f7842a867845f93d974b89e9db9d4ea6d468948
dibressonCosta <luizgustavo.costa@hotmail.com>
2021-05-25 23:46:34 -0300

    inicio deletar evento

--- agenda/src/components/Agenda.vue
+++ agenda/src/components/Agenda.vue
@@ -62,8 +62,8 @@
         >
           <v-card color="grey lighten-4" min-width="350px" flat>
             <v-toolbar :color="selectedEvent.color" dark>
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
+              <v-btn @click="deleteEvent(selectedEvent)" icon>
+                <v-icon>mdi-delete</v-icon>
               </v-btn>
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
               <v-spacer></v-spacer>
